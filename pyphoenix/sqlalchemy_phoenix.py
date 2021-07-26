412403e1ad8fc94d1ddc4b182758de629ed1a77c
melnikovmaksimv <73821572+melnikovmaksimv@users.noreply.github.com>
2021-07-26 14:54:50 +0300

    Update sqlalchemy_phoenix.py

--- pyphoenix/sqlalchemy_phoenix.py
+++ pyphoenix/sqlalchemy_phoenix.py
@@ -74,11 +74,11 @@ class PhoenixDialect(DefaultDialect):
         return connection.execute(query, params).first() is not None
 
     def get_schema_names(self, connection, **kw):
-        query = "SELECT DISTINCT CASE WHEN TABLE_SCHEM IS NULL THEN 'phoenix' ELSE TABLE_SCHEM END FROM SYSTEM.CATALOG"
+        query = "SELECT DISTINCT CASE WHEN TABLE_SCHEM IS NULL THEN '' ELSE TABLE_SCHEM END FROM SYSTEM.CATALOG"
         return [row[0] for row in connection.execute(query)]
 
     def get_table_names(self, connection, schema=None, **kw):
-        if schema is None or schema == 'phoenix':
+        if schema is None or schema == '':
             query = "SELECT DISTINCT table_name FROM SYSTEM.CATALOG"
             params = []
         else:
@@ -87,7 +87,7 @@ class PhoenixDialect(DefaultDialect):
         return [row[0] for row in connection.execute(query, params)]
 
     def get_columns(self, connection, table_name, schema=None, **kw):
-        if schema is None or schema == 'phoenix':
+        if schema is None or schema == '':
             query = "SELECT COLUMN_NAME,  DATA_TYPE, NULLABLE " \
                     "FROM system.catalog " \
                     "WHERE  UPPER(table_name) = ? " \
